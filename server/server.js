bb618321cbfa68fbe7bc86187f984b6459e5406a
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-06 22:49:46 -1000

    created routes folder
    -created year route
    -required year and using it as /year
    
    resolves #23

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const CONFIG = require('./config/config.json');
 const session = require('express-session');
+const year = require('./routes/year');
 
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
+app.use('/year', year);
 
 module.exports = app;
