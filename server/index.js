2673df5a668e47ab3f3922fad9b11b105ffd6c57
aukai84 <mtirrell@hawaii.edu>
2017-04-10 20:00:24 -1000

    Revert "created routes for crime, city, and state"

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const PORT = process.env.PORT || 8080;
-const server = require('./server');
-const db = require('./models');
-
-server.listen(PORT, () => {
-  console.log('listening on port ' + PORT)
-  db.sequelize.sync();
-})
