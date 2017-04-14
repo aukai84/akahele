87eb3a28528ecb9e2b49f21c121dbf57191454db
Danika Harada <danimi@Danikas-MacBook-Pro.local>
2017-04-13 19:07:16 -1000

    set up react

---
+++ client/src/container/homePage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="homePage">
+        <div className="App-header">
+        
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Hello World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './container/homePage.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root')
+);
