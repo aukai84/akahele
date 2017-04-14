7a9c7c32d9d00cb92e22034f9afd5454da38bb3c
Danika Harada <danimi@Danikas-MacBook-Pro.local>
2017-04-13 19:14:01 -1000

    issue 43 created react webpack resolved issue #43

--- client/src/container/homePage.jsx
+++ client/src/container/homePage.jsx
@@ -7,7 +7,7 @@ class HomePage extends Component {
       <div className="homePage">
         <div className="App-header">
         
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React!!</h2>
         </div>
         <p className="App-intro">
           Hello World
