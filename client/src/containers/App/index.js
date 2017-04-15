ffca3ed5901eb4b83e63d21d6747e63c7dfee3c9
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-15 13:59:25 -1000

    data from api showing on console

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import './index.css';
+import {requestHelper} from '../../lib/modules.js';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            honolulu: []
+        }
+    }
+
+    displayHonoluluData(){
+        requestHelper('GET', 'http://localhost:8080/cities/Honolulu/crime')
+        .then(data => {
+            console.log(data)
+            this.setState({
+                honolulu: this.state.honolulu.concat([data])
+            })
+        })
+    }
+
+    componentWillMount(){
+        this.displayHonoluluData();
+    }
   render() {
+        console.log(this.state)
+
     return (
       <div className="homePage">
         <div className="App-header">
