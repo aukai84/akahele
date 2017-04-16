4beee105b84e425a5384b41b82f99bd771847201
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-15 17:03:36 -1000

    mock map working

--- client/src/components/d3graph.js
+++ client/src/components/d3graph.js
@@ -2,7 +2,31 @@ import React, {Component} from 'react';
 import rd3 from 'react-d3-library';
 import node from '../lib/js/d3file.js';
 const RD3Component = rd3.Component;
-console.log("node ", node)
+const Map = require('react-d3-map').Map;
+const MarkerGroup = require('react-d3-map').MarkerGroup;
+
+ var width = 700;
+  var height = 700;
+  // set your zoom scale
+  var scale = 1200 * 5;
+  // min and max of your zoom scale
+  var scaleExtent = [1 << 12, 1 << 13]
+  // set your center point
+  var center = [122, 23.5];
+  // set your popupContent
+  var popupContent = function(d) { return d.properties.text; }
+
+  var data = {
+    "type": "Feature",
+    "properties": {
+      "text": "this is a Point!!!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [122, 23.5]
+    }
+}
+
 class ReactD3Component extends Component {
     constructor(props){
         super(props);
@@ -18,9 +42,26 @@ class ReactD3Component extends Component {
     }
 
     render(){
+        console.log(this.state.d3)
         return (
             <div>
                 <RD3Component data={this.state.d3}/>
+                <h1>TEST MAP</h1>
+                <Map
+                    width={width}
+                    height={height}
+                    scale={scale}
+                    scaleExtent={scaleExtent}
+                    center={center}
+                  >
+                    <MarkerGroup
+                      key={"polygon-test"}
+                      data={data}
+                      popupContent={popupContent}
+                      markerClass={"your-marker-css-class"}
+                    />
+                  </Map>
+                , document.getElementById('blank-point')
             </div>
         )
     }
