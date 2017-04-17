f9ff26d9825bd3d1460c7c03aaed156655951b62
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-16 17:56:32 -1000

    trying to get chroropleth to show up

--- client/src/components/d3-components/d3-us-map.js
+++ client/src/components/d3-components/d3-us-map.js
@@ -10,12 +10,65 @@ let width = 960;
 let height = 600;
 
 let topodata = require('../d3maps/usa/us.json');
-let unemploy = d3.tsvParse('unemployment.tsv');
-console.log('topodata ', topodata)
+let unemploy = d3.csvParse(`id,rate
+01,.097
+02,.091
+04,.088
+05,.121
+06,.099
+08,.164
+09,.167
+10,.108
+11,.06
+12,.118
+13,.099
+15,.127
+16,.17
+17,.08
+18,.104
+19,.085
+20,.114
+21,.107
+22,.14
+23,.101
+24,.097
+25,.096
+26,.093
+27,.211
+28,.07
+29,.09
+30,.06
+31,.107
+32,.05
+33,.123
+34,.12
+35,.147
+36,.127
+37,.099
+38,.089
+39,.018
+40,.107
+41,.148
+42,.105
+44,.136
+45,.086
+46,.093
+47,.05
+48,.08
+49,.075
+50,.08
+51,.02
+53,.092
+54,.111
+55,.187
+56,.152
+72,.092,
+`);
+
+console.log('unemploy ', unemploy)
 var dataStates = topojson.mesh(topodata, topodata.objects.states, function(a, b) { return a !== b; });
 
 let statePolygon = topojson.feature(topodata, topodata.objects.states).features;
-console.log(statePolygon)
 var domain = {
     scale: 'quantize',
     domain: [0, .15],
