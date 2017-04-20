9f502b7d53930a4d4f8d576190f4e55f8b3f53d1
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-19 20:47:31 -1000

    starting on bar graphs

---
+++ client/src/components/d3-components/bar-graph.js
@@ -0,0 +1,13 @@
+import * as Recharts from 'recharts';
+import React, {Component} from 'react';
+
+class SimpleBarGraph extends Component {
+    constructor(props){
+        super(props);
+    }
+
+
+
+
+    render(){}
+}
