64b5a909e657f213f9f3de72dd7dfaeddb4f7017
Nick Lee <nicklee808@gmail.com>
2017-04-26 21:51:22 -1000

    demo map displayed on react

---
+++ client/src/components/google-map/my_great_place.jsx
@@ -0,0 +1,19 @@
+import React, {PropTypes, Component} from 'react';
+
+import {greatPlaceStyle} from './my_great_place_styles.js';
+
+export default class MyGreatPlace extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  };
+
+  static defaultProps = {};
+
+  render() {
+    return (
+       <div style={greatPlaceStyle}>
+          {this.props.text}
+       </div>
+    );
+  }
+}
--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -3,41 +3,31 @@ import './index.css';
 import {retrieveData} from '../../lib/modules/modules.js';
 import ChartsContainer from '../../components/d3graph.js';
 import Sidebar from '../../components/Sidebar.jsx';
-
-
-
+import GoogleMaps from '../../components/google-map/simple_map_page.jsx';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            isModalOpen: false,
-            isModalGraphOpen: false
-        }
-
+  constructor(props){
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      isModalGraphOpen: false
     }
-
-
-
-  render() {
-
-
-        // console.log(this.state)
-        // console.log(this.props.graphType);
-    return (
-
-         <div className="bigContainer">
-            <Sidebar/>
-            <div className="main-container">
-                <h2>TESTING REACT-D3-LIBRARY</h2>
-               
-            </div>
-         </div>
-
-    );
   }
 
-
+  render() {
+  // console.log(this.state)
+  // console.log(this.props.graphType);
+    return (
+       <div className="bigContainer">
+        <Sidebar/>
+        <div className="main-container">
+          <h2>TESTING REACT-D3-LIBRARY</h2>
+          <GoogleMaps/>
+             
+        </div>
+       </div>
+    );
+  }
 }
 
 export default App;
