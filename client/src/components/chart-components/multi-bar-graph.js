fd51fbbaa333ef86f59c1021c2650b7717f3ce43
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-30 12:53:14 -1000

    multi bar graph filter by crime and slider

--- client/src/components/chart-components/multi-bar-graph.js
+++ client/src/components/chart-components/multi-bar-graph.js
@@ -10,22 +10,45 @@ const {BarChart, Bar, Brush, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip
 class MultiBarGraph extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            dataKey: 'murder_and_manslaughter',
+            crimeName: 'murder'
+        }
     }
 
-
+    crimeChange = (event) =>{
+        console.log('event ', event.target.value)
+        this.setState({
+            dataKey: event.target.value,
+            crimeName: event.target.value.split('_').join(' ')
+        })
+    }
 
     render(){
+        console.log('chart state ', this.state)
         return(
-            <BarChart className="multi-bar-graph" width={800} height={400} data={this.props.multiBarData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                <XAxis dataKey='city' />
-                <YAxis/>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <Tooltip/>
-                <Legend verticalAlign="top" wraperStyle={{lineHeight: '40px'}} />
-                <ReferenceLine y={0} stroke="#000" />
-                <Brush dataKey="city" height={30} stroke="#8884d8"/>
-                <Bar dataKey="murder_and_manslaughter" name="murder" fill="blue"/>
-            </BarChart>
+            <div>
+                 <select onChange={this.crimeChange}>
+                        <option value='murder_and_manslaughter'>murder</option>
+                        <option value='rape'>rape</option>
+                        <option value='robbery'>robbery</option>
+                        <option value='aggravated_assault'>aggravated assault</option>
+                        <option value="burglary">burglary</option>
+                        <option value='larceny_theft'>theft/larceny</option>
+                        <option value='motor_vehicle_theft'>motor vehicle theft</option>
+                        <option value='arson'>arson</option>
+                </select>
+                <BarChart className="multi-bar-graph" width={800} height={400} data={this.props.multiBarData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    <XAxis dataKey='city' />
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Legend verticalAlign="top" wraperStyle={{lineHeight: '40px'}} />
+                    <ReferenceLine y={0} stroke="#000" />
+                    <Brush dataKey="city" height={30} stroke="#8884d8"/>
+                    <Bar dataKey={this.state.dataKey} name={this.state.crimeName} fill="blue"/>
+                </BarChart>
+            </div>
             )
     }
 }
