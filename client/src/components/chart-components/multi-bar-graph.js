ae07aebbba686c94eeb57b54d6d3ca820a20e1b7
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-30 12:06:53 -1000

    using 'brush' charts with slider to zoom in :)

--- client/src/components/chart-components/multi-bar-graph.js
+++ client/src/components/chart-components/multi-bar-graph.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 
 // const {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 
-const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
+// const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
+
+const {BarChart, Bar, Brush, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 
 class MultiBarGraph extends Component {
     constructor(props){
@@ -14,21 +16,16 @@ class MultiBarGraph extends Component {
 
     render(){
         return(
-            <LineChart className="multi-bar-graph" width={800} height={400} data={this.props.multiBarData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <BarChart className="multi-bar-graph" width={800} height={400} data={this.props.multiBarData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                 <XAxis dataKey='city' />
                 <YAxis/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip/>
-                <Legend />
-                <Line dataKey="murder_and_manslaughter" name="murder" fill="#8884d8" />
-                <Line dataKey="rape" name="rape" fill="#8884d8" />
-                <Line dataKey="robbery" name="robbery" fill="#8884d8" />
-                <Line dataKey="aggravated_assaultr" name="aggravated assault" fill="#8884d8" />
-                <Line dataKey="burglary" name="burglary" fill="#8884d8" />
-                <Line dataKey="larceny_theft" name="theft/larceny" fill="#8884d8" />
-                <Line dataKey="motor_vehicle_theft" name="motor vehicle theft" fill="#8884d8" />
-                <Line dataKey="arson" name="arson" fill="#8884d8" />
-            </LineChart>
+                <Legend verticalAlign="top" wraperStyle={{lineHeight: '40px'}} />
+                <ReferenceLine y={0} stroke="#000" />
+                <Brush dataKey="city" height={30} stroke="#8884d8"/>
+                <Bar dataKey="murder_and_manslaughter" name="murder" fill="blue"/>
+            </BarChart>
             )
     }
 }
