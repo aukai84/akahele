e10545854ce7486abc2f08dcbd1a3229cbabf02d
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-02-16 22:21:49 -1000

    prettied index.js code for readability

--- client/src/index.js
+++ client/src/index.js
@@ -7,30 +7,32 @@ import GoogleMaps from './components/google-map';
 
 
 import {
-	BrowserRouter as Router,
-	Route,
-	Link
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch
 } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <Router>
-  <div>
-  <div className="nav-div">
-    
-           <div className="home-nav-button"> <Link to="/">Akahele</Link></div>
-
-           <div className="other-nav-buttons"><Link to="/">Home</Link> 
-            <Link to="/nation">| Nation Map</Link>
-            <Link to="/local">| Local View</Link></div>
-
-    
-  </div>
-  <Route exact path="/" component={ HomePage } />
-  <Route path="/nation" component={ App } />
-<Route path="/local" component={ GoogleMaps } />
-</div>
-
+    <Router>
+        <div>
+          <div className="nav-div">
+              <div className="home-nav-button">
+                  <Link to="/">Akahele</Link>
+              </div>
+              <div className="other-nav-buttons">
+                  <Link to="/">Home</Link> 
+                  <Link to="/nation">| Nation Map</Link>
+                  <Link to="/local">| Local View</Link>
+              </div>
+          </div>
+          <Switch>
+              <Route exact path="/" component={ HomePage } />
+              <Route path="/nation" component={ App } />
+              <Route path="/local" component={ GoogleMaps } />
+          </Switch>
+        </div>
   </Router>,
   document.getElementById('root')
 );
